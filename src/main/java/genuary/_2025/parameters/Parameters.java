48d3efa1478d152fa1a8711d62043e97dcacf8ea
Sebastien Bissay <seb.bissay@gmail.com>
2024-12-19 22:21:50 +0100

    feature: start sketch

--- src/main/java/genuary/_2025/parameters/Parameters.java
+++ src/main/java/genuary/_2025/parameters/Parameters.java
@@ -1,4 +1,4 @@
-package parameters;
+package genuary._2025.parameters;
 
 import java.lang.reflect.Field;
 import java.util.Collections;
@@ -6,19 +6,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class Parameters {
-    public static final long SEED = 11;
+    public static final long SEED = 20250101;
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 1000;
+    public static final float NOISE_SCALE = 1 / 200f;
+    public static final Color BACKGROUND_COLOR = new Color(220);
+    public static final Color STROKE_COLOR = new Color(24, 11, 5, 120);
 
     /**
-     * Helper method to extract the constants in order to save them to a json file
+     * Helper method to extract the constants in order to genuary._2025.save them to a json file
      *
      * @return a Map of the constants (name -> value)
      */
-    public static Map<String, ?> toJsonMap() throws IllegalAccessException {
+    public static Map<String, Object> toJsonMap() throws IllegalAccessException {
         Map<String, Object> map = new HashMap<>();
 
         Field[] declaredFields = Parameters.class.getDeclaredFields();
         for(Field field : declaredFields) {
-            field.setAccessible(true);
             map.put(field.getName(), field.get(Parameters.class));
         }
 
